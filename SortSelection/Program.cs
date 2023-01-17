61abf9e4b3dca2e1588854dbdf3f130f83bacd85
Alex-tech <code.box@bk.ru>
2023-01-17 19:31:10 +0300

    Сделана сортировка методом

--- SortSelection/Program.cs
+++ SortSelection/Program.cs
@@ -4,6 +4,24 @@ void ShowArray(int[]array)
     System.Console.WriteLine($"{String.Join(", ", array)}");
 }
 
-int[] collectin = {2, 34, 6, 09, 5, 6, 3, 1, 9};
-ShowArray(collectin);
+void SortCollection(int[]array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int pos = i;
+         for (int j = i+1; j < array.Length; j++)
+         {
+            if (array[j] < array[pos]) pos = j;
+         }
+        int temp = array[i];
+        array[i] = array[pos];
+        array[pos] = temp;
+    }
+}
+
+int[] collection = {2, 34, 6, 09, 5, 6, 3, 1, 9};
+ShowArray(collection);
+SortCollection(collection);
+ShowArray(collection);
+
 
