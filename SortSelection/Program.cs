02bb4f15d236d0b6072fa1daa9769a4429acbab6
Alex-tech <code.box@bk.ru>
2023-01-17 19:01:26 +0300

    Первый вывод массива через метод

---
+++ SortSelection/Program.cs
@@ -0,0 +1,9 @@
+﻿// Создать массив и сортировать его по возрастанию
+void ShowArray(int[]array)
+{
+    System.Console.WriteLine($"{String.Join(", ", array)}");
+}
+
+int[] collectin = {2, 34, 6, 09, 5, 6, 3, 1, 9};
+ShowArray(collectin);
+
