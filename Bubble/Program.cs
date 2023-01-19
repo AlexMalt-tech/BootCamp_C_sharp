bfcdde6ea13203290a647f89f288d7adc03c6be4
Alex <67325048+AlexMalt-tech@users.noreply.github.com>
2023-01-19 22:28:47 +0300

    Работа из дома. Создание случайного масссива

---
+++ Bubble/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.ObjectModel;
+// Создать массив и сортировать его по возрастанию
+
+int[] numSet = new int[9];
+
+for (int i = 0; i < numSet.Length; i++)
+{
+	numSet[i] = new Random().Next(1, 100);
+}
+
+System.Console.WriteLine(String.Join(", ", numSet));
